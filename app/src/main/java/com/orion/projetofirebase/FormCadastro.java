4b80e43dbb9bd55e4e12f08baa86fb46be318739
Gilberto-Silva <gilberto.eletro@gmail.com>
2023-05-23 20:48:16 -0300

    Primeiro Commit - Aula 1

---
+++ app/src/main/java/com/orion/projetofirebase/FormCadastro.java
@@ -0,0 +1,18 @@
+package com.orion.projetofirebase;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FormCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_cadastro);
+
+        getSupportActionBar().hide();
+
+    }
+
+}
---
+++ app/src/main/java/com/orion/projetofirebase/TelaPrincipal.java
@@ -0,0 +1,14 @@
+package com.orion.projetofirebase;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TelaPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_principal);
+    }
+}
